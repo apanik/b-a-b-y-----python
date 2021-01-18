3ca5be16bf92a9bdfcc54982d3381552207d825f
apanik <ap.anik71@gmail.com>
2021-01-18 22:43:31 +0600

    baby snake taking polymarphism to github

---
+++ oop-polymarphism/builtin-polymarphism.py
@@ -0,0 +1,37 @@
+# Defination 
+
+"""
+
+The word polymorphism means having many forms. 
+In programming, polymorphism means same function name 
+(but different signatures) being uses for different types.
+
+"""
+
+# Example 01
+# len fuction
+print(len("Anik"))
+print((len[1,2,3,4,5,6,7,"aa","ab","ac"]))
+
+# here same function length but being used for getting different types
+
+# Example 02
+# Custom Method/Function
+def add(x,y,z=0):
+	print(x+y+z)
+
+add(5,10)
+add(5,10,15)
+
+# here also same function length but being used for getting different types
+
+
+ # Example 03
+ # built in sum function
+
+ print(sum(10,11))
+ print(sum(10,11,12))
+
+
+
+
